853605abd0e733b709e1adb074c6ac57058124f4
felgus <felix@pixotag.com>
2018-12-19 17:20:56 +0100

    Gjort 1.4

--- uppgift_ocenaografi.m
+++ uppgift_ocenaografi.m
@@ -6,12 +6,13 @@ F = 2.6*10^(-5);
 A = 0.05^2 * pi;
 
 % Model loop starting from ground level
-     h(1)=0; t(1)=0;
-     for kk=1:200;
-t(kk+1)=t(kk)+dt;
-    
+h(1)=0; t(1)=0;
 
-% timestepping equation
-h(kk+1)=h(kk)+dt*(F/A);
+for kk=1:200;
+    t(kk+1)=t(kk)+dt;
+    
+    % timestepping equation
+    h(kk+1)=h(kk)+dt*(F/A);
 end
+
 plot(t,h); xlabel ('Time in seconds'), ylabel ('Water level in M');
